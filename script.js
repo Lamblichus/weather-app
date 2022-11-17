3920e4522899c779e09fb17b1e9636057c053679
Faruk <118258606+Lamblichus@users.noreply.github.com>
2022-11-17 19:23:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let getWeather = () => {
   }
   //If input field is NOT empty
   else {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    let url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={8be790d38faddd2d405d3b15b398fa5a}";
     //Clear the input field
     cityRef.value = "";
     fetch(url)
