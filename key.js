963d3b7d23430eea28c5cf8c8ae0e02655443a4e
Faruk <118258606+Lamblichus@users.noreply.github.com>
2022-11-17 19:20:33 +0300

    Update key.js

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
 //Assign the copied API key to the 'key' variable
-key = "8be790d38faddd2d405d3b15b398fa5a";
+key = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={8be790d38faddd2d405d3b15b398fa5a}";
